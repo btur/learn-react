b00076fc8378132618d1448a6e1e5fa4783c866b
btur <budianto.turisno@gmail.com>
2019-07-04 20:46:54 +0700

    change hashrouter to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import ProductList from './pages/ProductList'
 import Chat from './pages/Chat'
 
@@ -9,11 +9,11 @@ import Chat from './pages/Chat'
 class App extends React.Component {
   render() {
     return (
-    <HashRouter>
+    <BrowserRouter>
       <Route path="/" exact component={Home} />
       <Route path="/products" exact component={ProductList} />
       <Route path="/chat" exact component={Chat} />
-    </HashRouter>
+    </BrowserRouter>
     );
   }
 }
